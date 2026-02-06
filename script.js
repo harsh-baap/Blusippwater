a6a9f139be9405bdeb5cc033e9de142f5ca5fcd6
Harsh <h8434885@gmail.com>
2026-02-06 23:48:07 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("order").addEventListener("click", function () {
+  const name = document.getElementById("input").value.trim();
+  const address = document.getElementById("address").value.trim();
+  const quantity = document.getElementById("quantity").value.trim();
+  const mobile = document.getElementById("mobile").value.trim();
+
+  if (!name || !address || !quantity || !mobile) {
+    alert("Please fill in all details before ordering.");
+    return;
+  }
+  const whatsappNumber = "917498886372";
+
+  const message = `Hello Blusipp Water %0A
+    I would like to place an order.%0A
+    Name: ${name}%0A
+    Address: ${address}%0A
+    Quantity: ${quantity}%0A
+    Mobile: ${mobile}`;
+
+  const whatsappURL = `https://wa.me/${whatsappNumber}?text=${message}`;
+
+  window.open(whatsappURL, "_blank");
+});
