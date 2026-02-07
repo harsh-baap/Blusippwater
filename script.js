3120b00087aa6a706a575fa58000762d8d9d9d11
Harsh <h8434885@gmail.com>
2026-02-07 11:06:06 +0530

    Update WhatsApp number and order message format

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ document.getElementById("order").addEventListener("click", function () {
   const name = document.getElementById("input").value.trim();
   const address = document.getElementById("address").value.trim();
   const quantity = document.getElementById("quantity").value.trim();
+  const qty500 = document.getElementById("qty500").value.trim();
   const mobile = document.getElementById("mobile").value.trim();
 
   if (!name || !address || !quantity || !mobile) {
     alert("Please fill in all details before ordering.");
     return;
   }
-  const whatsappNumber = "919823847901";
+  const whatsappNumber = "917498886372";
 
   const message = `Hello Blusipp Water %0A
     I would like to place an order.%0A
     Name: ${name}%0A
     Address: ${address}%0A
-    Quantity: ${quantity}%0A
+    Qty 1Ltr: ${quantity || 0}%0A
+    Qty 500ml: ${qty500 || 0}%0A
     Mobile: ${mobile}`;
 
   const whatsappURL = `https://wa.me/${whatsappNumber}?text=${message}`;
